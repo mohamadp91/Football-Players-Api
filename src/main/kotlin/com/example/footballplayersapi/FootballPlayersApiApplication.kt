448ecd7f17f137194af355bb181cff4889a28aaa
mohamadp91 <69380537+mohamadp91@users.noreply.github.com>
2023-04-21 17:26:38 +0330

    init the project (#1)

---
+++ src/main/kotlin/com/example/footballplayersapi/FootballPlayersApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.footballplayersapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FootballPlayersApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<FootballPlayersApiApplication>(*args)
+}
