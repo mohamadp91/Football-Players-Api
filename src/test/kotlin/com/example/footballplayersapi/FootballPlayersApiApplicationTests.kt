abdf7f01cd498f57d4b7c2debe041186d4aecc5b
mohamadp91 <mohamad7291m@gmail.com>
2023-04-21 17:26:08 +0330

    init the project

---
+++ src/test/kotlin/com/example/footballplayersapi/FootballPlayersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.footballplayersapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FootballPlayersApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
